c72aa2388e33019ba206bf1767ee420539feec09
Oliver Woodings <oliver.woodings@qubitdigital.com>
2015-01-08 13:58:54 +0000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+/*
+ * grunt-tex-pdflatex
+ * https://github.com/grunt-tex/grunt-tex-pdflatex
+ *
+ * Copyright (c) 2015 Oliver Woodings
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        "Gruntfile.js",
+        "tasks/*.js",
+        "<%= mochaTest.test.src %>"
+      ],
+      options: {
+        jshintrc: ".jshintrc"
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ["tmp"]
+    },
+
+    // Need to ensure directory is created before tests are run
+    mkdir: {
+      tests: {
+        options: {
+          create: ["tmp"]
+        }
+      }
+    },
+
+    // Configuration to be run (and then tested).
+    pdflatex: {
+      options: {
+        args: {
+          "output-directory": "tmp"
+        }
+      },
+      succeed: {
+        files: [
+          { src: "test/fixtures/succeed.tex" }
+        ]
+      },
+      fail: {
+        files: [
+          { src: "test/fixtures/fail.tex" }
+        ]
+      }
+    },
+
+    nodeunit: {
+      tests: ["test/*_test.js"]
+    },
+
+    // Unit tests.
+    mochaTest: {
+      test: {
+        options: {
+          reporter: "spec"
+        },
+        src: ["test/test.js"]
+      }
+    }
+
+  });
+
+  // Actually load this plugins task(s).
+  grunt.loadTasks("tasks");
+
+  // Load NPM tasks
+  require("load-grunt-tasks")(grunt);
+
+  // Whenever the 'test' task is run, first clean the 'tmp' dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask("test", ["clean", "mkdir", "mochaTest"]);
+
+  // By default, lint and run all tests.
+  grunt.registerTask("default", ["jshint", "test"]);
+
+};
---
+++ tasks/pdflatex.js
@@ -0,0 +1,91 @@
+/*
+ * grunt-tex-pdflatex
+ * https://github.com/grunt-tex/grunt-tex-pdflatex
+ *
+ * Copyright (c) 2015 Oliver Woodings
+ * Licensed under the MIT license.
+ */
+
+var when = require("when");
+var lift = require("when/node").lift;
+var exec = lift(require("exec"));
+var _    = require("lodash-node");
+var path = require("path");
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask("pdflatex", "Grunt pdflatex task", function() {
+
+    var done = this.async();
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      executable: "pdflatex"
+    });
+
+    // Collate arguments for pdflatex
+    var defaultArgs = {
+      "interaction": "nonstopmode",
+      "file-line-error": null
+    };
+    var argsObj = _.extend(defaultArgs, options.args);
+    var args = _.map(argsObj, function (value, key) {
+      var argString = "-" + key;
+      if (value !== null) {
+        argString += "=" + value;
+      }
+      return argString;
+    });
+    args.unshift(options.executable);
+
+    // Iterate over all specified files, executing pdflatex on them
+    var promises = this.filesSrc.map(function (file) {
+      var opts = {
+        cwd: path.dirname(file)
+      };
+      return exec([].concat([], args, [file]))
+        .spread(function (out, code) {
+          // Return results as an object
+          return {
+            file: file,
+            out: out,
+            code: code
+          };
+        });
+    });
+
+    // When all files have been processed, indicate task has finished
+    when.all(promises).then(function (results) {
+      results.forEach(processResult);
+      done();
+    });
+
+  });
+
+  function processResult(result) {
+    if (result.code === 0) {
+      grunt.log.ok("Compiled " + result.file);
+    } else {
+      var out = result.out.split("\n");
+
+      _.chain(result.out.split("\n"))
+        .filter(function (line) {
+          return /^(.+?)\:(\d+)\: /.test(line);
+        })
+        .map(function (line) {
+          var parts = line.match(/^(.+?)\:(\d+)\: (.+)$/);
+          return {
+            file: parts[1],
+            line: parts[2],
+            msg: parts[3]
+          };
+        })
+        .each(function (error) {
+          grunt.log.error(error.file + ":" + error.line + " " + error.msg);
+        });
+
+      grunt.fail.fatal("Unable to compile " + result.file);
+    }
+  }
+
+};
